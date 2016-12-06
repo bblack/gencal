386cce768007884ca5cc60f242fee1caca920858
Brian Black <black@gatech.edu>
2016-12-06 13:32:59 -0500

    test table for may 2016

--- test/index.js
+++ test/index.js
@@ -168,5 +168,35 @@ describe('GenCal', function(){
         assert(cells.find((td) => td.innerText == '31'));
       })
     })
+    context('May 2016', function(){
+      beforeEach(function(){
+        var month = new GenCal.Month(2016, 4);
+        table = new GenCal.Table(month, document.createElement('table'));
+      })
+      it('should contain 5 rows of 7 cells', function(){
+        var rows = table.getElementsByTagName('tr');
+        assert.equal(rows.length, 5);
+        for (var i = 0; i < rows.length; i++) {
+          var cells = rows[i].getElementsByTagName('td');
+          assert.equal(cells.length, 7);
+        }
+      })
+      it('should have a number in every cell', function(){
+        var cells = table.getElementsByTagName('td');
+        for (var i = 0; i < cells.length; i++) {
+          assert(cells[i].innerText.match(/\d+/));
+        }
+      })
+      it('should have a 1 in the first row', function(){
+        var firstRow = table.getElementsByTagName('tr').item(0);
+        var cells = Array.from(firstRow.getElementsByTagName('td'));
+        assert(cells.find((td) => td.innerText == '1'));
+      })
+      it('should have a 31 in the last row', function(){
+        var lastRow = table.getElementsByTagName('tr').item(4);
+        var cells = Array.from(lastRow.getElementsByTagName('td'));
+        assert(cells.find((td) => td.innerText == '31'));
+      })
+    })
   })
 })
