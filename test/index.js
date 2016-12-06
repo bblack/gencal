328d5df324c36e1efcf045d38e6575518e7d49af
Brian Black <black@gatech.edu>
2016-12-06 10:28:27 -0500

    test May 2016 which starts on Sunday

--- test/index.js
+++ test/index.js
@@ -36,6 +36,36 @@ describe('GenCal', function(){
         })
       })
     })
+    describe('initialized to May 2016', function(){
+      var month = new GenCal.Month(2016, 4);
+      describe('.weeks()', function(){
+        var weeks = month.weeks();
+        it('should return an array of arrays of 7 dates', function(){
+          assert(Array.isArray(weeks));
+          weeks.forEach((week) => {
+            assert.equal(week.length, 7);
+            week.forEach((d) => assert(Date.prototype.isPrototypeOf(d)));
+          })
+        })
+        it('should start each week with Sunday', function(){
+          weeks.forEach((week) => {
+            assert.equal(week[0].getDay(), 0);
+          })
+        })
+        it('should start at Sunday, May 1 2016', function(){
+          assert(weeks[0][0].getTime() == new Date(2016, 4, 1).getTime());
+        })
+        it('should end at Saturday, Jun 4 2016', function(){
+          var lastWeek = weeks[weeks.length - 1];
+          assert(lastWeek[6].getTime() == new Date(2016, 5, 4).getTime());
+        })
+        it('should contain May 31 in the last week', function(){
+          var lastOfMonth = new Date(2016, 4, 31);
+          var lastWeek = weeks[weeks.length - 1];
+          assert(lastWeek.find((d) => d.getTime() == lastOfMonth.getTime()));
+        })
+      })
+    })
   })
 })
 
